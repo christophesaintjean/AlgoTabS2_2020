49ec85f459bf736dec2b758f5adc28d1e3d4d188
Christophe Saint-Jean <christophe.saint-jean@univ-lr.fr>
2020-01-15 14:34:20 +0100

    Codes du cours 2

---
+++ code/CM2_insert.py
@@ -0,0 +1,32 @@
+from tqdm import tqdm_gui
+import random
+from time import time
+
+
+for _ in tqdm_gui(range(10**8)):
+    val = random.randint(0, 100)
+    L.append(val)
+
+
+for _ in tqdm_gui(range(10**6)):
+    pos = random.randint(0, len(L))  ## insertion avant
+    val = random.randint(0, 100)
+    L.insert(pos, val)
+
+
+## Astuce
+L = []
+debut = time()
+for i in range(10**3):
+    L.insert(0, i)
+fin = time()
+duree = fin - debut
+print("Duree Insert: ", duree)
+L = []
+debut = time()
+for i in range(10**3):
+    L.append(i)
+L.reverse()
+fin = time()
+duree = fin - debut
+print("Duree Append+Reverse: ", duree)
